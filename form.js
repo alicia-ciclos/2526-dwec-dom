47ac3ab23b191151699de54f67b6597883716c92
Alice Wyan <121136+wyan@users.noreply.github.com>
2025-11-04 15:50:31 +0100

    Ejercicio DOM formularios

---
+++ form.js
@@ -0,0 +1,44 @@
+"use strict"
+
+const form = [
+    {
+        "tag": "input",
+        "type": "text",
+        "id": "username",
+        "name": "username",
+        "class": "user-input",
+        "label": "Identificador de usuario",
+    },
+    {
+        "tag": "input",
+        "type": "email",
+        "id": "email",
+        "name": "email",
+        "class": "email-input",
+        "label": "Correo electrónico",
+    },
+    {
+        "tag": "input",
+        "type": "password",
+        "id": "password",
+        "name": "password",
+        "class": "password-input",
+        "label": "Contraseña",
+    },
+    {
+        "tag": "input",
+        "type": "password",
+        "id": "confirm_password",
+        "name": "confirm_password",
+        "class": "password-input",
+        "label": "Confirmar contraseña",
+    },
+    {
+        "tag": "button",
+        "id": "submit-btn",
+        "class": "submit-button",
+        "value": "Crear usuario",
+    }
+]
+
+
